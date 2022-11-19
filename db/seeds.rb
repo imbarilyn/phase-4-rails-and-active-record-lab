dfcab1efc643736c4855dd52067ebff7441f0edc
Linah Imbari <linahmuhonjaimbari@gmail.com>
2022-11-19 16:19:40 +0300

    Completed assignment

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,6 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+Student.create!(first_name: "Dwight", last_name: "Schrute", grade: 2)
+Student.create!(first_name: "Chris", last_name: "Ngoma", grade: 3)
+Student.create!(first_name: "Dylan", last_name: "Chysler", grade: 5)
